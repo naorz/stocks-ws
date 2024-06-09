45f4916d28c41e5deec16e86e990e8ba4c6c93bf
oren meiron <oren.meiron@darrow.ai>
2024-06-09 10:22:13 +0300

    tmp

--- src/app/components/WebSocketClient.tsx
+++ src/app/components/WebSocketClient.tsx
@@ -2,16 +2,19 @@
 import { useEffect, useRef } from "react";
 
 function WebSocketClient() {
-  let socket = new WebSocket("ws://localhost:8080");
+  const socket = useRef<any>(null);
 
   useEffect(() => {
+    // Create WebSocket connection.
+    socket.current = new WebSocket("ws://localhost:8080");
+
     // Connection opened
-    socket.addEventListener("open", () => {
+    socket.current.addEventListener("open", () => {
       console.log("Connected to the WebSocket server at ws://localhost:8080");
     });
 
     return () => {
-      socket.close();
+      socket.current.close();
     };
   }, []);
 
